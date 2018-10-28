21f85a9a76d6c6f9ba5e7e99eea6b8302aa8a6fe
sungang <330267780@qq.com>
2018-10-28 21:09:54 +0800

    打印样式修改

--- src/main/java/com/yada/enjoyIntegral/controller/TranListController.java
+++ src/main/java/com/yada/enjoyIntegral/controller/TranListController.java
@@ -39,170 +39,199 @@ import com.yada.security.service.OrgManager;
 @Controller
 public class TranListController {
 
-	@Autowired
-	private TranListManager tranListManager;
-	@Autowired
-	private OrgManager OrgManager;
-	@Autowired
-	private ProductCartManager productCartManager;
+    @Autowired
+    private TranListManager tranListManager;
+    @Autowired
+    private OrgManager OrgManager;
+    @Autowired
+    private ProductCartManager productCartManager;
 
-	@RequestMapping
-	public String list(Model model, @ModelAttribute("query") TranListQuery query,HttpServletRequest request) {
-		HttpSession session = request.getSession();
-		User user = (User) session.getAttribute("curUser");
-		String orgId = user.getUserExt().getOrgId();
-		
-		String nowDate = new SimpleDateFormat("yyyyMMdd").format(new Date());
-		//撤销的条件：1.已经消费且交易成功  2.是当天的交易  3. 撤销操作只可以由做这笔交易的机构进行，其他任何机构都不可以
-		query.setOrgId(orgId);
-		//交易状态00：表示消费  流水状态00：表示交易成功
-		query.setTranState("00");
-		query.setState("00");
-		query.setTranDate(nowDate);
+    @RequestMapping
+    public String list(Model model, @ModelAttribute("query") TranListQuery query, HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("curUser");
+        String orgId = user.getUserExt().getOrgId();
 
-		Page page = tranListManager.queryPage(query);
-		String message = query.getMessage();
-		if(message!=null){
-			if(message.equals("success")){
-				query.setMessage("撤销成功！！");
-			}
-			else if(message.equals("false")){
-				query.setMessage("撤销失败！！但是由于故障流水未记录");
-			}
-		}
-			
-		model.addAttribute("page", page);
-		model.addAttribute("query", query);
-		return "/enjoyIntegral_pages/TranList/list";
-	}
-	
-	@RequestMapping
-	public String listPrint(Model model, @ModelAttribute("query") TranListQuery query,HttpServletRequest request) {
-		HttpSession session = request.getSession();
-		User user = (User) session.getAttribute("curUser");
-		String orgId = user.getUserExt().getOrgId();
-		
-		String nowDate = new SimpleDateFormat("yyyyMMdd").format(new Date());
-		
-		query.setOrgId(orgId);
-		query.setState("00");
-		query.setTranState("00");
-		Page page = tranListManager.queryPage(query);
-		model.addAttribute("page", page);
-		model.addAttribute("orgId", orgId);
-		model.addAttribute("nowDate", nowDate);
-		model.addAttribute("query", query);
-		return "/enjoyIntegral_pages/TranList/listPrint";
-	}
+        String nowDate = new SimpleDateFormat("yyyyMMdd").format(new Date());
+        //撤销的条件：1.已经消费且交易成功  2.是当天的交易  3. 撤销操作只可以由做这笔交易的机构进行，其他任何机构都不可以
+        query.setOrgId(orgId);
+        //交易状态00：表示消费  流水状态00：表示交易成功
+        query.setTranState("00");
+        query.setState("00");
+        query.setTranDate(nowDate);
 
-	@RequestMapping
-	public String revoke (Model model,String tranId,String newTranId){
-		String message="";
-		try {
-			TranList oldTranList = new TranList();
-			oldTranList.setTranId(tranId);
+        Page page = tranListManager.queryPage(query);
+        String message = query.getMessage();
+        if (message != null) {
+            if (message.equals("success")) {
+                query.setMessage("撤销成功！！");
+            } else if (message.equals("false")) {
+                query.setMessage("撤销失败！！但是由于故障流水未记录");
+            }
+        }
 
-			TranList newTranList = new TranList();
-			newTranList.setTranId(newTranId);
-			tranListManager.updateTranLists(oldTranList,newTranList);
+        model.addAttribute("page", page);
+        model.addAttribute("query", query);
+        return "/enjoyIntegral_pages/TranList/list";
+    }
 
-			message="success";
-		} catch (Exception e) {
-			message="false";
-			e.printStackTrace();
-		}
-		return "redirect:list.do?message="+message;
-	}
-	
-	@RequestMapping
-	public void AJAX_getPrintContent(HttpServletRequest request,
-									 HttpServletResponse response,String tranId) {
-		HttpSession session = request.getSession();
-		// StringBuilder sb = new StringBuilder();
-		User user = (User) session.getAttribute("curUser");
-		String userId = user.getLoginName();
-		String userName = user.getUserName();
-		String orgId = user.getUserExt().getOrgId();
-		Org org = OrgManager.getById(orgId);
-		Org orgF = OrgManager.getById(org.getPorgId());
-		org.setName("管辖机构:"+orgF.getName()+"   " +
-				"<br><br>兑换机构:"+org.getName()+"<br><br>经办人:");
-		List<TranList> tranLists = tranListManager.findTranListByTranId(tranId);
+    @RequestMapping
+    public String listPrint(Model model, @ModelAttribute("query") TranListQuery query, HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("curUser");
+        String orgId = user.getUserExt().getOrgId();
 
-		String name = tranLists.get(0).getCustomerName();
-		String certificateNo =  tranLists.get(0).getCertificateNo();
-		String phone =  tranLists.get(0).getCustomerPhone();
-		String certificateType =  tranLists.get(0).getCertificateType();
-		List<ProductCart> productCartsPrint = new ArrayList<ProductCart>();
-		for(TranList tranList : tranLists){
-			ProductCart productCart = productCartManager.getById(tranList.getTranId()+tranList.getProductId());
-			productCartsPrint.add(productCart);
-		}
-		// 响应
-		try {
-			PrintWriter out = response.getWriter();
-			PageUtil.writePage(out, name, certificateNo, phone,
-					certificateType, productCartsPrint,userId,userName,org.getName());
-			// out.print(sb.toString());
-			out.flush();
-			out.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+        String nowDate = new SimpleDateFormat("yyyyMMdd").format(new Date());
 
-	/**
-	 * @Author      ： zhangQiang
-	 * @Date        : 2018/4/3-16:16
-	 * @Desc        : 发起撤销交易时，先复制之前的交易数据，向库中插入之前的交易数据，并将tran_state置为撤销，流水状态state为空
-	 * @param response:
-	 * @param tranId:
-	 */
-	@RequestMapping
-	public void AJAX_addTranList(HttpServletResponse response,String tranId) {
-		String uuid = "";
-		try {
+        query.setOrgId(orgId);
+        query.setState("00");
+        query.setTranState("00");
+        Page page = tranListManager.queryPage(query);
+        model.addAttribute("page", page);
+        model.addAttribute("orgId", orgId);
+        model.addAttribute("nowDate", nowDate);
+        model.addAttribute("query", query);
+        return "/enjoyIntegral_pages/TranList/listPrint";
+    }
 
-			//添加撤销交易的流水
-			 uuid = UUID.randomUUID().toString().replace("-", "");
-			tranListManager.addRevokeTranList(uuid,tranId);
-		}catch (Exception e){
-			e.printStackTrace();
-		}
-		//将uuid返回给页面,uuid将做为这笔撤销交易的标识
-		response.setContentType("text/html;charset=utf-8");
-		try {
-			response.getWriter().print(uuid);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    @RequestMapping
+    public String revoke(Model model, String tranId, String newTranId) {
+        String message = "";
+        try {
+            TranList oldTranList = new TranList();
+            oldTranList.setTranId(tranId);
 
-	/**
-	 * @Author      ： zhangQiang
-	 * @Date        : 2018/4/10-13:11
-	 * @Desc        : 展示交易的流水数据
-	 * @param model:
-	 * @param tranId: 交易流水编号
-	 */
-	@RequestMapping
-	public String show(Model model,String tranId){
-		List<TranList> tranListByTranId = tranListManager.findTranListByTranId(tranId);
-		model.addAttribute("list",tranListByTranId);
-		return "/enjoyIntegral_pages/TranList/show";
-	}
+            TranList newTranList = new TranList();
+            newTranList.setTranId(newTranId);
+            tranListManager.updateTranLists(oldTranList, newTranList);
 
-	@RequestMapping
-	public String revokeDefeat(String integral,String tranId){
-		//撤销失败，更新原交易流水状态
-		try{
-			TranList tranList = new TranList();
-			tranList.setTranState(integral);
-			tranList.setTranId(tranId);
-			tranListManager.update(tranList);
-		}catch (Exception e){
-			e.printStackTrace();
-		}
-		return "/enjoyIntegral_pages/TranList/list";
-	}
+            message = "success";
+        } catch (Exception e) {
+            message = "false";
+            e.printStackTrace();
+        }
+        return "redirect:list.do?message=" + message;
+    }
+
+    @RequestMapping
+    public void AJAX_getPrintContent(HttpServletResponse response, String tranId) {
+
+        List<TranList> tranLists = tranListManager.findTranListByTranId(tranId);
+
+        String name = tranLists.get(0).getCustomerName();
+        String certificateNo = tranLists.get(0).getCertificateNo();
+        String phone = tranLists.get(0).getCustomerPhone();
+        String certificateType = tranLists.get(0).getCertificateType();
+        List<ProductCart> productCartsPrint = new ArrayList<ProductCart>();
+        for (TranList tranList : tranLists) {
+            ProductCart productCart = productCartManager.getById(tranList.getTranId() + tranList.getProductId());
+            productCartsPrint.add(productCart);
+        }
+        // 响应
+        try {
+            PrintWriter out = response.getWriter();
+
+            PageUtil.writePageNew(out, name, certificateNo, phone,
+                    certificateType, productCartsPrint);
+
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    @RequestMapping
+    public void AJAX_getPrintContent1(HttpServletRequest request,
+                                      HttpServletResponse response, String tranId) {
+        HttpSession session = request.getSession();
+        // StringBuilder sb = new StringBuilder();
+        User user = (User) session.getAttribute("curUser");
+        String userId = user.getLoginName();
+        String userName = user.getUserName();
+        String orgId = user.getUserExt().getOrgId();
+        Org org = OrgManager.getById(orgId);
+        Org orgF = OrgManager.getById(org.getPorgId());
+        org.setName("管辖机构:" + orgF.getName() + "   " +
+                "<br><br>兑换机构:" + org.getName() + "<br><br>经办人:");
+        List<TranList> tranLists = tranListManager.findTranListByTranId(tranId);
+
+        String name = tranLists.get(0).getCustomerName();
+        String certificateNo = tranLists.get(0).getCertificateNo();
+        String phone = tranLists.get(0).getCustomerPhone();
+        String certificateType = tranLists.get(0).getCertificateType();
+        List<ProductCart> productCartsPrint = new ArrayList<ProductCart>();
+        for (TranList tranList : tranLists) {
+            ProductCart productCart = productCartManager.getById(tranList.getTranId() + tranList.getProductId());
+            productCartsPrint.add(productCart);
+        }
+        // 响应
+        try {
+            PrintWriter out = response.getWriter();
+            PageUtil.writePage(out, name, certificateNo, phone,
+                    certificateType, productCartsPrint, userId, userName, org.getName());
+            // out.print(sb.toString());
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param response:
+     * @param tranId:
+     * @Author ： zhangQiang
+     * @Date : 2018/4/3-16:16
+     * @Desc : 发起撤销交易时，先复制之前的交易数据，向库中插入之前的交易数据，并将tran_state置为撤销，流水状态state为空
+     */
+    @RequestMapping
+    public void AJAX_addTranList(HttpServletResponse response, String tranId) {
+        String uuid = "";
+        try {
+
+            //添加撤销交易的流水
+            uuid = UUID.randomUUID().toString().replace("-", "");
+            tranListManager.addRevokeTranList(uuid, tranId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //将uuid返回给页面,uuid将做为这笔撤销交易的标识
+        response.setContentType("text/html;charset=utf-8");
+        try {
+            response.getWriter().print(uuid);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param model:
+     * @param tranId: 交易流水编号
+     * @Author ： zhangQiang
+     * @Date : 2018/4/10-13:11
+     * @Desc : 展示交易的流水数据
+     */
+    @RequestMapping
+    public String show(Model model, String tranId) {
+        List<TranList> tranListByTranId = tranListManager.findTranListByTranId(tranId);
+        model.addAttribute("list", tranListByTranId);
+        return "/enjoyIntegral_pages/TranList/show";
+    }
+
+    @RequestMapping
+    public String revokeDefeat(String integral, String tranId) {
+        //撤销失败，更新原交易流水状态
+        try {
+            TranList tranList = new TranList();
+            tranList.setTranState(integral);
+            tranList.setTranId(tranId);
+            tranListManager.update(tranList);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "/enjoyIntegral_pages/TranList/list";
+    }
 }
--- src/main/webapp/WEB-INF/pages/enjoyIntegral_pages/TranList/listPrint.jsp
+++ src/main/webapp/WEB-INF/pages/enjoyIntegral_pages/TranList/listPrint.jsp
@@ -30,7 +30,16 @@
 
 </style>
 <script type="text/javascript">
-	
+    //同时打印的商户数量，目前是15个，如果更多可添加convertPage+商户个数变量
+    var confirmPage,convertPage0,convertPage1,convertPage2,convertPage3,convertPage4,convertPage5,convertPage6,convertPage7,convertPage8,convertPage9,convertPage11,convertPage12,convertPage13,convertPage14;
+
+    require([ "dijit/Dialog", "dijit/form/TextBox", "dijit/form/Button" ]);
+    var init = function() {
+        printDialog.hide();
+        dojo.connect(printDialog, "onCancel", function(e){
+            exitPrint()
+        });
+    }
 
 	function queryPage() {
 		var queryForm = document.getElementById("queryForm");
@@ -52,40 +61,66 @@
                 tranId : tranId
 			},
 			load : function(data) {
-				printPage(data);
+                var printDate = data.split("-confirmPage-");
+                confirmPage = printDate[0];
+                var convertDate = printDate[1].split("-convertPage-");
+                convertPage0=convertDate[0];
+                convertPage1=convertDate[1];
+                convertPage2=convertDate[2];
+                convertPage3=convertDate[3];
+                convertPage4=convertDate[4];
+                convertPage5=convertDate[5];
+                convertPage6=convertDate[6];
+                convertPage7=convertDate[7];
+                convertPage8=convertDate[8];
+                convertPage9=convertDate[9];
+                convertPage10=convertDate[9];
+                convertPage11=convertDate[11];
+                convertPage12=convertDate[12];
+                convertPage13=convertDate[13];
+                convertPage14=convertDate[14];
+                data = data.substring(0, data.lastIndexOf("%"));
+                document.getElementById("printDetail").innerHTML=data.replace(/-confirmPage-/g,"").replace(/-convertPage-/g,"").replace(/-printButton-/g,"");
+                printDialog.show();
+                dojo.style("mask", {
+                    "display" : "block"
+                });
 			},
 			error : function(error) {
 				alert(error);
 			}
 		});
 	}
-	
-	function printPage(data){
-		 if (confirm('确定生成打印单吗？')) { 
-			var oPop = window.open('','oPop'); 
-			data=data.substring(0,data.lastIndexOf("%"));
-			var ctype = '/EnjoyIntegral/css/dy.css';
-			 var s = document.createElement('link');  
-		    s.setAttribute('type','text/css');  
-		    s.setAttribute('rel','stylesheet');  
-		    s.setAttribute('href',ctype);  
-		    var head = document.getElementsByTagName('head');  
-		    head[0].appendChild(s);  
-			var pageCode = '<!DOCTYPE html>';
-			pageCode += '<html>';
-			pageCode +='<head>';  
-			pageCode +='<link rel="stylesheet" type="text/css" href="'+ctype+'">';  
-			pageCode +='</head>';  
-			pageCode +='<body>'; 
-			pageCode +='<div class="dy">'; 
-			pageCode += data; 
-			pageCode +='</div>';  
-			pageCode +='</body>';  
-			pageCode +='</html>';
-			oPop.document.write(pageCode);  
-			oPop.print();
-		 }
-	}
+
+    function printPage(data) {
+        if (confirm('确定生成打印单吗？')) {
+            var oPop = window.open('', 'oPop');
+            var ctype = '/EnjoyIntegral/css/dy.css';
+            var s = document.createElement('link');
+            s.setAttribute('type', 'text/css');
+            s.setAttribute('rel', 'stylesheet');
+            s.setAttribute('href', ctype);
+            var head = document.getElementsByTagName('head');
+            head[0].appendChild(s);
+            var pageCode = '<!DOCTYPE html>';
+            pageCode += '<html>';
+            pageCode += '<head>';
+            pageCode += '<link rel="stylesheet" type="text/css" href="'+ctype+'">';
+            pageCode += '</head>';
+            pageCode += '<body>';
+            pageCode += data.split("-printButton-")[0];
+            pageCode += '</body>';
+            pageCode += '</html>';
+            oPop.document.write(pageCode);
+            oPop.print();
+        }
+    }
+
+    function exitPrint(){
+        dojo.style("mask", {
+            "display" : "none"
+        });
+    }
 </script>
 </head>
 <body class="claro">
@@ -255,6 +290,18 @@
 							onclick="printPage()" /><span id="mess" style="color: red;" /></td>
 					</tr>
 				</table>
+			</div>
+			<div data-dojo-type="dijit/Dialog" data-dojo-id="printDialog"
+				 title="单据打印">
+				<div id="printDetail"></div>
+				<table class="b">
+					<tr align="center">
+						<td><center><input type="button" style="width: 80px" value="关闭" onclick="exitPrint()"/></center></td>
+					</tr>
+				</table>
+			</div>
+			<div id="mask" class="mask">
+				<div class="bg"></div>
 			</div>
 			<simpletable:pageToolbar page="${page}">
 			</simpletable:pageToolbar>
